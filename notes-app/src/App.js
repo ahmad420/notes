cb98ce783463237a5750ec9019a9844314732f9f
Ahmad <ahmdaf420@gmail.com>
2023-01-17 18:23:52 +0200

    creating main pages& components,router setup ,mui setup

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LogIn from "./pages/LogIn";
+import Register from "./pages/Register";
+import Notes from "./pages/Notes";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index element={<LogIn />} />
+          <Route path="register" element={<Register />} />
+          <Route path="notes" element={<Notes />} />
+          <Route path="main" element={<Main />} />
+        
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
